bab74688512e71230e3e5cb568db9cd67085b7ae
CaptainVidia <46515050+CaptainVidia@users.noreply.github.com>
2020-09-18 13:49:57 -0500

    player/enemy movement working

--- Project Lunar Shooter/Assets/Scripts/PlayerMove.cs
+++ Project Lunar Shooter/Assets/Scripts/PlayerMove.cs
@@ -2,17 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour
+public class PlayerMove : TacticsMove
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    // Use this for initialization
+
+    void Start ()
+	{
+        Init();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.DrawRay(transform.position, transform.forward);
+
+        if (!turn)
+        {
+            return;
+        }
+
+        if (!moving)
+        {
+            FindSelectableTiles();
+            CheckMouse();
+        }
+        else
+        {
+            Move();
+        }
+    }
+
+    void CheckMouse()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.tag == "Tile")
+                {
+                    Tile t = hit.collider.GetComponent<Tile>();
+
+                    if (t.selectable)
+                    {
+                        MoveToTile(t);
+                    }
+                }
+            }
+        }
     }
 }
--- Project Lunar Shooter/Assets/Scripts/Tile.cs
+++ Project Lunar Shooter/Assets/Scripts/Tile.cs
@@ -4,27 +4,30 @@
                Checks if tiles are occupied
 */
 
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    public bool walkable = false;
+    public bool walkable = true;
     public bool current = false;
     public bool target = false;
     public bool selectable = false;
 
     public List<Tile> adjacencyList = new List<Tile>();
 
+    //Needed BFS (breadth first search)
     public bool visited = false;
     public Tile parent = null;
-    // forcing player or enemy to move a selected amt of distance
     public int distance = 0;
 
+    //For A*
+    public float f = 0;
+    public float g = 0;
+    public float h = 0;
 
-    // Start is called before the first frame update
+    // Use this for initialization
     void Start()
     {
 
@@ -33,23 +36,18 @@ public class Tile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // show player's current position
         if (current)
         {
             GetComponent<Renderer>().material.color = Color.magenta;
         }
-
-        // shows move possibility
         else if (target)
         {
-            GetComponent<Renderer>().material.color = Color.black;
+            GetComponent<Renderer>().material.color = Color.green;
         }
-        // shows move selected that can be made
         else if (selectable)
         {
             GetComponent<Renderer>().material.color = Color.red;
         }
-        // default to original shade of grass or white
         else
         {
             GetComponent<Renderer>().material.color = Color.white;
@@ -59,54 +57,46 @@ public class Tile : MonoBehaviour
     public void Reset()
     {
         adjacencyList.Clear();
-         current = false;
-         target = false;
-         selectable = false;
 
-         visited = false;
-         parent = null;
-         // forcing player or enemy to move a selected amt of distance
-         distance = 0;
+        current = false;
+        target = false;
+        selectable = false;
 
+        visited = false;
+        parent = null;
+        distance = 0;
+
+        f = g = h = 0;
     }
 
-    // jumpHeight = how many players they can jump up or down
-    public void FindNeighbors(float jumpHeight)
+    public void FindNeighbors(float jumpHeight, Tile target)
     {
         Reset();
 
-        CheckTile(Vector3.forward, jumpHeight);
-        //opposite function
-        CheckTile(-Vector3.forward, jumpHeight);
-        //z axis
-        CheckTile(Vector3.right, jumpHeight);
-        CheckTile(-Vector3.right, jumpHeight);
+        CheckTile(Vector3.forward, jumpHeight, target);
+        CheckTile(-Vector3.forward, jumpHeight, target);
+        CheckTile(Vector3.right, jumpHeight, target);
+        CheckTile(-Vector3.right, jumpHeight, target);
     }
-    
 
-    public void CheckTile(Vector3 direction, float jumpHeight)
+    public void CheckTile(Vector3 direction, float jumpHeight, Tile target)
     {
-        // finding tiles
-        Vector3 halfExtends = new Vector3(0.25f,(1+jumpHeight)/2.0f,0.25f);
-        Collider[] colliders = Physics.OverlapBox(transform.position + direction, halfExtends);
+        Vector3 halfExtents = new Vector3(0.25f, (1 + jumpHeight) / 2.0f, 0.25f);
+        Collider[] colliders = Physics.OverlapBox(transform.position + direction, halfExtents);
 
         foreach (Collider item in colliders)
         {
-            // checking for tiles
             Tile tile = item.GetComponent<Tile>();
             if (tile != null && tile.walkable)
             {
                 RaycastHit hit;
-                //testing if there something on tie, if something dont add, if nothing then we add it
-                if (!Physics.Raycast(tile.transform.position, Vector3.up, out hit, 1))
+
+                if (!Physics.Raycast(tile.transform.position, Vector3.up, out hit, 1) || (tile == target))
                 {
                     adjacencyList.Add(tile);
                 }
             }
         }
     }
-    
 }
-
-
 
