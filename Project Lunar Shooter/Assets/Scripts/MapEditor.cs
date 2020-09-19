a429faa056874869b141c055eb1003dccf66f20a
John Mordi <johnmordi00@gmail.com>
2020-09-19 14:44:41 -0500

    Revert "map update"
    This reverts commit 6a46a278e230b97532b9fb1058ff6aaa20cfbd2e.

--- Project Lunar Shooter/Assets/Scripts/MapEditor.cs
+++ Project Lunar Shooter/Assets/Scripts/MapEditor.cs
@@ -7,14 +7,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-//[CustomEditor (typeof (MapGenerator))]
-//public class MapEditor : Editor
-//{
-//    public override void OnInspectorGUI()
-//    {
-//        base.OnInspectorGUI();
+[CustomEditor (typeof (MapGenerator))]
+public class MapEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
 
-//        MapGenerator map = target as MapGenerator;
-//        map.GenerateMap();
-//    }
-//}
+        MapGenerator map = target as MapGenerator;
+        map.GenerateMap();
+    }
+}
