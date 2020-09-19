3d65725e129e673ce7435dcf76502fb5198930b9
John Mordi <johnmordi00@gmail.com>
2020-09-19 14:44:36 -0500

    Revert "player/enemy movement working"
    This reverts commit bab74688512e71230e3e5cb568db9cd67085b7ae.

--- Project Lunar Shooter/Assets/Scripts/Tile.cs
+++ Project Lunar Shooter/Assets/Scripts/Tile.cs
@@ -4,30 +4,27 @@
                Checks if tiles are occupied
 */
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    public bool walkable = true;
+    public bool walkable = false;
     public bool current = false;
     public bool target = false;
     public bool selectable = false;
 
     public List<Tile> adjacencyList = new List<Tile>();
 
-    //Needed BFS (breadth first search)
     public bool visited = false;
     public Tile parent = null;
+    // forcing player or enemy to move a selected amt of distance
     public int distance = 0;
 
-    //For A*
-    public float f = 0;
-    public float g = 0;
-    public float h = 0;
 
-    // Use this for initialization
+    // Start is called before the first frame update
     void Start()
     {
 
@@ -36,18 +33,23 @@ public class Tile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // show player's current position
         if (current)
         {
             GetComponent<Renderer>().material.color = Color.magenta;
         }
+
+        // shows move possibility
         else if (target)
         {
-            GetComponent<Renderer>().material.color = Color.green;
+            GetComponent<Renderer>().material.color = Color.black;
         }
+        // shows move selected that can be made
         else if (selectable)
         {
             GetComponent<Renderer>().material.color = Color.red;
         }
+        // default to original shade of grass or white
         else
         {
             GetComponent<Renderer>().material.color = Color.white;
@@ -57,46 +59,54 @@ public class Tile : MonoBehaviour
     public void Reset()
     {
         adjacencyList.Clear();
+         current = false;
+         target = false;
+         selectable = false;
 
-        current = false;
-        target = false;
-        selectable = false;
+         visited = false;
+         parent = null;
+         // forcing player or enemy to move a selected amt of distance
+         distance = 0;
 
-        visited = false;
-        parent = null;
-        distance = 0;
-
-        f = g = h = 0;
     }
 
-    public void FindNeighbors(float jumpHeight, Tile target)
+    // jumpHeight = how many players they can jump up or down
+    public void FindNeighbors(float jumpHeight)
     {
         Reset();
 
-        CheckTile(Vector3.forward, jumpHeight, target);
-        CheckTile(-Vector3.forward, jumpHeight, target);
-        CheckTile(Vector3.right, jumpHeight, target);
-        CheckTile(-Vector3.right, jumpHeight, target);
+        CheckTile(Vector3.forward, jumpHeight);
+        //opposite function
+        CheckTile(-Vector3.forward, jumpHeight);
+        //z axis
+        CheckTile(Vector3.right, jumpHeight);
+        CheckTile(-Vector3.right, jumpHeight);
     }
+    
 
-    public void CheckTile(Vector3 direction, float jumpHeight, Tile target)
+    public void CheckTile(Vector3 direction, float jumpHeight)
     {
-        Vector3 halfExtents = new Vector3(0.25f, (1 + jumpHeight) / 2.0f, 0.25f);
-        Collider[] colliders = Physics.OverlapBox(transform.position + direction, halfExtents);
+        // finding tiles
+        Vector3 halfExtends = new Vector3(0.25f,(1+jumpHeight)/2.0f,0.25f);
+        Collider[] colliders = Physics.OverlapBox(transform.position + direction, halfExtends);
 
         foreach (Collider item in colliders)
         {
+            // checking for tiles
             Tile tile = item.GetComponent<Tile>();
             if (tile != null && tile.walkable)
             {
                 RaycastHit hit;
-
-                if (!Physics.Raycast(tile.transform.position, Vector3.up, out hit, 1) || (tile == target))
+                //testing if there something on tie, if something dont add, if nothing then we add it
+                if (!Physics.Raycast(tile.transform.position, Vector3.up, out hit, 1))
                 {
                     adjacencyList.Add(tile);
                 }
             }
         }
     }
+    
 }
+
+
 
