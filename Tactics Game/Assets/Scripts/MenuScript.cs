2fe18e46881a292ea5bdbe4ba6d0df3e7e17d62c
John Mordi <johnmordi00@gmail.com>
2020-09-19 15:02:06 -0500

    Revert "blah"
    This reverts commit 36d7ab7e6359e449e606f965bea80f7911850578, reversing
    changes made to 56bf2ac9ee705a91c15cb91b263127f2dc4d7d5a.

---
+++ Tactics Game/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,35 @@
+﻿/*Author: George Tang
+ * Assignment: Project Lunar Shooter
+ * Desciption: Creates a menu to easily load material and scripts to a 12 by 12 grid
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MenuScript
+{
+    [MenuItem("Tools/Assign Tile Material")]
+    public static void AssignTileMaterial()
+    {
+        GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
+        Material material = Resources.Load<Material>("Tile");
+
+        foreach (GameObject t in tiles)
+        {
+            t.GetComponent<Renderer>().material = material;
+        }
+    }
+
+    [MenuItem("Tools/Assign Tile Script")]
+    public static void AssignTileScript()
+    {
+        GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
+
+        foreach (GameObject t in tiles)
+        {
+            t.AddComponent<Tile>();
+        }
+    }
+}
