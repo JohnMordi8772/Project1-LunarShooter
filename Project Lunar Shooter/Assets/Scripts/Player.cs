ab106e9015e9387992b93797eac7aa1d957316c7
John Mordi <johnmordi00@gmail.com>
2020-09-19 19:03:18 -0500

    Altered player script and scene

--- Project Lunar Shooter/Assets/Scripts/Player.cs
+++ Project Lunar Shooter/Assets/Scripts/Player.cs
@@ -5,27 +5,29 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float horizontalInput;
-    public float speed = 10.0f;
-    private float xRange = 14;
+    public float speed = 100.0f;
+    private float xUpperRange = 11;
+    private float xLowerRange = 0;
 
     // Update is called once per frame
     void Update()
     {
         // horizontal input from right and left arrow or A ad D keys
-        horizontalInput = Input.GetAxis("Horizontal");
+        horizontalInput = Input.GetAxisRaw("Horizontal");
 
         //transform horizontally with input
-        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        if(Input.GetButtonDown("Horizontal"))
+            transform.Translate(Vector3.right * horizontalInput);
 
         //keep player in bounds
-        if (transform.position.x < -xRange)
+        if (transform.position.x < -xLowerRange)
         {
-            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xLowerRange, transform.position.y, transform.position.z);
         }
 
-        if (transform.position.x > xRange)
+        if (transform.position.x > xUpperRange)
         {
-            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+            transform.position = new Vector3(xUpperRange, transform.position.y, transform.position.z);
         }
     }
 }
