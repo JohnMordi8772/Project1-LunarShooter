f0dcf3d937162897af905b9bb6be374525779d01
Josh Bumbalough <jbumb9000@gmail.com>
2020-09-19 19:50:06 -0500

    Added Attacks
    Added ability of player to shoot light, medium, and heavy prefabs.

---
+++ Project Lunar Shooter/Assets/Scripts/PlayerShootPrefab.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShootPrefab : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject lightAttack;
+    public GameObject mediumAttack;
+    public GameObject heavyAttack;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            Instantiate(lightAttack, transform.position, lightAttack.transform.rotation);
+        }
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            Instantiate(mediumAttack, transform.position, mediumAttack.transform.rotation);
+        }
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            Instantiate(heavyAttack, transform.position, heavyAttack.transform.rotation);
+        }
+    }
+}
