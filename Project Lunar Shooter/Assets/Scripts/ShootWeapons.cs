2bd93ae7a948c338e99d68db3de9a9c5066f515c
CaptainVidia <46515050+CaptainVidia@users.noreply.github.com>
2020-09-20 01:21:54 -0500

    player script / shooting

---
+++ Project Lunar Shooter/Assets/Scripts/ShootWeapons.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootWeapons : MonoBehaviour
+{
+    public GameObject Beam;
+    public GameObject Pistol;
+    public GameObject Laser;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Instantiate(Beam, transform.position, Beam.transform.rotation);
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            Instantiate(Pistol, transform.position, Pistol.transform.rotation);
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            Instantiate(Laser, transform.position, Laser.transform.rotation);
+        }
+
+    }
+}
