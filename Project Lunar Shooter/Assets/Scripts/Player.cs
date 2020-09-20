f439010a265d620b28b26ee134c6a1f1112ec2e6
CaptainVidia <46515050+CaptainVidia@users.noreply.github.com>
2020-09-20 01:23:58 -0500

    Revert "player shoot revise"
    This reverts commit 189df0327e3efc27328ac2d558e98f4f24141a69, reversing
    changes made to 2bd93ae7a948c338e99d68db3de9a9c5066f515c.

--- Project Lunar Shooter/Assets/Scripts/Player.cs
+++ Project Lunar Shooter/Assets/Scripts/Player.cs
@@ -5,29 +5,27 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float horizontalInput;
-    public float speed = 100.0f;
-    private float xUpperRange = 11;
-    private float xLowerRange = 1;
+    public float speed = 10.0f;
+    private float xRange = 14;
 
     // Update is called once per frame
     void Update()
     {
         // horizontal input from right and left arrow or A ad D keys
-        horizontalInput = Input.GetAxisRaw("Horizontal");
+        horizontalInput = Input.GetAxis("Horizontal");
 
         //transform horizontally with input
-        if(Input.GetButtonDown("Horizontal"))
-            transform.Translate(Vector3.right * horizontalInput);
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
         //keep player in bounds
-        if (transform.position.x < xLowerRange)
+        if (transform.position.x < -xRange)
         {
-            transform.position = new Vector3(xLowerRange, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
 
-        if (transform.position.x > xUpperRange)
+        if (transform.position.x > xRange)
         {
-            transform.position = new Vector3(xUpperRange, transform.position.y, transform.position.z);
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
     }
 }
