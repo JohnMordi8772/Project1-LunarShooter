8d9a9cae8002e13fbed472a618a0d2e7e1e9b2b2
Josh Bumbalough <jbumb9000@gmail.com>
2020-09-20 16:08:20 -0500

    Added TurnManager
    Added TurnManager script

--- Project Lunar Shooter/Assets/Scripts/Player.cs
+++ Project Lunar Shooter/Assets/Scripts/Player.cs
@@ -9,22 +9,31 @@ public class Player : MonoBehaviour
     private float xUpperRange = 11;
     private float xLowerRange = 1;
 
+
     // Update is called once per frame
     void Update()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal"); 
-
-        if(Input.GetButtonDown("Horizontal"))
-            transform.Translate(Vector3.right * horizontalInput);
+        
+        if(TurnManager.isPlayerTurn && TurnManager.actionPoints > 0)
+        {
+            if(Input.GetButtonDown("Horizontal"))
+            {
+                transform.Translate(Vector3.right * horizontalInput);
+                TurnManager.actionPoints--;
+            }
+        }
 
         //keep player in bounds
         if (transform.position.x < xLowerRange)
         { 
             transform.position = new Vector3(xLowerRange, transform.position.y, transform.position.z);
+            TurnManager.actionPoints++;
         }
         if (transform.position.x > xUpperRange)
         {
             transform.position = new Vector3(xUpperRange, transform.position.y, transform.position.z);
+            TurnManager.actionPoints++;
         }
     }
 }
---
+++ Project Lunar Shooter/Assets/Scripts/TurnManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnManager : MonoBehaviour
+{
+    public static bool isPlayerTurn; 
+    public static float actionPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPlayerTurn = false;
+        actionPoints = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            isPlayerTurn = !isPlayerTurn;
+            actionPoints = 10;
+        }
+
+
+    }
+}
