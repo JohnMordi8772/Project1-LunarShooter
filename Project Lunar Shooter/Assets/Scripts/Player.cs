ad730a05cf9d3d47738c1b022fdf8eedd0875c7f
CaptainVidia <46515050+CaptainVidia@users.noreply.github.com>
2020-09-20 02:09:32 -0500

    visual representation shoot prefabs

--- Project Lunar Shooter/Assets/Scripts/Player.cs
+++ Project Lunar Shooter/Assets/Scripts/Player.cs
@@ -5,27 +5,26 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float horizontalInput;
-    public float speed = 10.0f;
-    private float xRange = 14;
+    public float speed = 100.0f;
+    private float xUpperRange = 11;
+    private float xLowerRange = -1;
+    private float xLowerRange2 = 1;
 
     // Update is called once per frame
     void Update()
     {
-        // horizontal input from right and left arrow or A ad D keys
-        horizontalInput = Input.GetAxis("Horizontal");
-
-        //transform horizontally with input
-        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * horizontalInput);
 
         //keep player in bounds
-        if (transform.position.x < -xRange)
+        if (transform.position.x < -xLowerRange)
+            if (transform.position.x < xLowerRange2)
+            {
+                transform.position = new Vector3(-xLowerRange, transform.position.y, transform.position.z);
+                transform.position = new Vector3(xLowerRange2, transform.position.y, transform.position.z);
+            }
+        if (transform.position.x > xUpperRange)
         {
-            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
-        }
-
-        if (transform.position.x > xRange)
-        {
-            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+            transform.position = new Vector3(xUpperRange, transform.position.y, transform.position.z);
         }
     }
 }
---
+++ Project Lunar Shooter/Assets/Scripts/PlayerShootingPrefabs.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShootingPrefabs : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    // beam
+    public GameObject lightAttack;
+    // pistol
+    public GameObject mediumAttack;
+    // laser
+    public GameObject heavyAttack;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Instantiate(lightAttack, transform.position, lightAttack.transform.rotation);
+            //Instantiate(lightAttack, new Vector3(transform.position.x, transform.position.y, transform.position.z + 1), lightAttack.transform.rotation);
+            // causes player to shoot two items ^
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Instantiate(mediumAttack, transform.position, mediumAttack.transform.rotation);
+            //Instantiate(mediumAttack, new Vector3(transform.position.x, transform.position.y, transform.position.z + 1), mediumAttack.transform.rotation);
+            // causes player to shoot two items ^
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Instantiate(heavyAttack, transform.position, heavyAttack.transform.rotation);
+            //Instantiate(heavyAttack, new Vector3(transform.position.x, transform.position.y, transform.position.z + 1), heavyAttack.transform.rotation);
+            // causes player to shoot two items ^
+        }
+    }
+}
+
