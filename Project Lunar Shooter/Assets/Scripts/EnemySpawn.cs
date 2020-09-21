062d74544e657e1add99a944e671b89719ef88f6
7riLo <lwyant@mail.bradley.edu>
2020-09-20 20:07:23 -0500

    added enemy spawn
    added enemy spawn

---
+++ Project Lunar Shooter/Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+
+    public GameObject[] enemyArray;
+    float randx;
+    Vector3 whereToSpawn;
+    /* The spawn rate needs to be based on a bool of if player turn or not*/
+    //public float spawnRate = 2f; 
+    float nextSpawn = 0.0f;
+    private float spawnCount;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!TurnManager.isPlayerTurn)
+        {
+            if (spawnCount <= 3)
+            {
+
+
+                int index = Random.Range(0, 3);
+                // nextSpawn = Time.time + spawnRate;   //BASED ON IF PLAYER TURN OR NOT
+                randx = Random.Range(1, 12);
+                //make is to set spawn position of z would be 11
+
+
+                whereToSpawn = new Vector3(randx, 1, 11);
+                Instantiate(enemyArray[index], whereToSpawn, Quaternion.identity);
+
+                spawnCount++;
+            }
+
+        }
+        else
+        {
+            spawnCount = 0;
+        }
+    }
+}
