3dc411812d54ad5eb22acbd3f3103e0788db6459
Josh Bumbalough <jbumb9000@gmail.com>
2020-10-04 13:29:45 -0500

    Added sounds for weapons
    Sounds for weapons

--- Project Lunar Shooter/Assets/Scripts/PlayerShootingPrefabs.cs
+++ Project Lunar Shooter/Assets/Scripts/PlayerShootingPrefabs.cs
@@ -17,6 +17,16 @@ public class PlayerShootingPrefabs : MonoBehaviour
     // laser
     public GameObject heavyAttack;
 
+    private AudioSource playerAudio;
+    public AudioClip pistolSound;
+    public AudioClip laserSound;
+    public AudioClip beamSound;
+
+    void Start()
+    {
+        playerAudio = GetComponent<AudioSource>();
+    }
+
 
     // Update is called once per frame
     void Update()
@@ -28,6 +38,7 @@ public class PlayerShootingPrefabs : MonoBehaviour
                     //does 3 dmg to one enemy, so it has a low cost. also, the projectile always fires one space in front of the player so that out of bounds functions properly.
                     Instantiate(lightAttack, new Vector3(transform.position.x, transform.position.y, transform.position.z + 1), lightAttack.transform.rotation);
                     TurnManager.actionPoints--;
+                    playerAudio.PlayOneShot(pistolSound, 1.0f);
                 }
             if (Input.GetKeyDown(KeyCode.W) && TurnManager.actionPoints >= 2)
                 {
@@ -36,12 +47,14 @@ public class PlayerShootingPrefabs : MonoBehaviour
                     Instantiate(mediumAttack, new Vector3(transform.position.x + 1, transform.position.y, transform.position.z + 1), mediumAttack.transform.rotation);
                     Instantiate(mediumAttack, new Vector3(transform.position.x - 1, transform.position.y, transform.position.z + 1), mediumAttack.transform.rotation);
                     TurnManager.actionPoints -= 2;
+                    playerAudio.PlayOneShot(laserSound, 1.0f);
                 }
             if (Input.GetKeyDown(KeyCode.E) && TurnManager.actionPoints >= 3)
                 {
                     //does one dmg to all enemies in column, so it costs the most
                     Instantiate(heavyAttack, new Vector3(transform.position.x, transform.position.y, transform.position.z + 1), heavyAttack.transform.rotation);
                     TurnManager.actionPoints -= 3;
+                    playerAudio.PlayOneShot(beamSound, 1.0f);
                 }
         }
        
