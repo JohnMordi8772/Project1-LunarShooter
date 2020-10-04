500d23ede305c5c7137ab1caae2c73a64ef87081
Josh Bumbalough <jbumb9000@gmail.com>
2020-10-04 16:50:04 -0500

    minor

--- Project Lunar Shooter/Assets/Scripts/ProjectileOnContact.cs
+++ Project Lunar Shooter/Assets/Scripts/ProjectileOnContact.cs
@@ -11,6 +11,7 @@ public class ProjectileOnContact : MonoBehaviour
 {
     public float dmg;
 
+
     private void OnTriggerEnter(Collider other)
     {
         other.gameObject.GetComponent<DestroyOutOfBounds>().health -= dmg;
@@ -19,8 +20,8 @@ public class ProjectileOnContact : MonoBehaviour
         {
             Destroy(other.gameObject);
             Destroy(gameObject);
-            FindObjectOfType<DestroyOutOfBounds>().explosionParticle.Play();
-            FindObjectOfType<DestroyOutOfBounds>().enemyAudio.PlayOneShot(FindObjectOfType<DestroyOutOfBounds>().explosionSound, 1.0f);
+            //FindObjectOfType<DestroyOutOfBounds>().explosionParticle.Play();
+            //FindObjectOfType<DestroyOutOfBounds>().enemyAudio.PlayOneShot(FindObjectOfType<DestroyOutOfBounds>().explosionSound, 1.0f);
             EnemySpawn.enemiesKilled++;
         }
         else
